566e6c429a49e137520121760999350674438cd3
daniel conteh <contehdaniel1995@gmail.com>
2022-08-12 15:41:02 +0000

    first commit

--- home/settings.py
+++ home/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-5nq@1e8v+#iw6mtj21^b8lw60t&%9@-upt6s70ef&^zgc5k()^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['django-natours.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-natours.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
